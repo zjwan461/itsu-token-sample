4e719f0516d850ec7fb63517c8cf1d8bf0e4cf5a
suben007 <suben@formssi.com>
2020-12-11 14:10:44 +0800

    更新sample示例

--- src/main/java/com/itsu/token/sample/checker/MyCustomTokenChecker.java
+++ src/main/java/com/itsu/token/sample/checker/MyCustomTokenChecker.java
@@ -1,9 +1,6 @@
 package com.itsu.token.sample.checker;
 
-import javax.annotation.Resource;
-
 import org.aspectj.lang.JoinPoint;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.itsu.itsutoken.checker.CusTokenChecker;
 import com.itsu.itsutoken.exception.TokenCheckException;
@@ -11,15 +8,9 @@ import com.itsu.token.sample.vo.MyCustomTableSample;
 
 public class MyCustomTokenChecker extends CusTokenChecker<MyCustomTableSample> {
 
-	@Resource
-	public JdbcTemplate jdbcTemplate;
-
 	@Override
 	public void check(JoinPoint joinPoint) throws TokenCheckException {
 		// do some check here
-		String name = this.getTableSample().getClass().getName();
-		System.out.println(name);
-
 	}
 
 	@Override
--- src/main/java/com/itsu/token/sample/vo/MyCustomTableSample.java
+++ src/main/java/com/itsu/token/sample/vo/MyCustomTableSample.java
@@ -1,6 +1,7 @@
 package com.itsu.token.sample.vo;
 
 import com.itsu.itsutoken.annotation.TableDesc;
+import com.itsu.itsutoken.annotation.TableField;
 import com.itsu.itsutoken.annotation.TableId;
 import com.itsu.itsutoken.table.TableSample;
 
@@ -9,4 +10,7 @@ public class MyCustomTableSample implements TableSample {
 
 	@TableId
 	private String id;
+	
+	@TableField("custom_field")
+	private String token;
 }
